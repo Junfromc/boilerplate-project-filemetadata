f82802ca668cf1b891016382302d3d400c3106a6
Junfromc <101047429+Junfromc@users.noreply.github.com>
2025-06-28 17:25:21 +0800

    finished the assignment

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
+"use strict";
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const uploadDest = "./public/uploads/";
+const upload = multer({ dest: uploadDest });
 var app = express();
+const fs = require("fs");
+const path = require("path");
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "no file uploaded" });
+  }
+  const fileInfo = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: `${req.file.size} bytes`,
+  };
+  console.log('req.file:', req.file);
+  fs.rm(req.file.path, {force: true},(err)=>{
+    if (err) throw err;
+    console.log('Temporary file deleted successfully');
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
